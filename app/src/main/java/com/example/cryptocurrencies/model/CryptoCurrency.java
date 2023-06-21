0bdb3a6ff741104ae4cccbe118758d3f95502f61
roopa <roopa.peddinti@improve10x.com>
2023-06-21 10:28:40 +0530

    Crypto Currencies Project Created.

---
+++ app/src/main/java/com/example/cryptocurrencies/model/CryptoCurrency.java
@@ -0,0 +1,12 @@
+package com.example.cryptocurrencies.model;
+
+public class CryptoCurrency {
+    private String id;
+    private String name;
+    private String symbol;
+    private Integer rank;
+    private Boolean isNew;
+    private Boolean isActive;
+    private String type;
+
+}
